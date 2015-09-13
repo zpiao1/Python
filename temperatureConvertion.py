63f8becfe44d1763a2a4c376ce1a6d90c2a272af
Chiu King Yee <zpiao1@gmail.com>
2015-09-13 19:31:37 +0800

    temperatureConvertion.py version 1

--- temperatureConvertion.py
+++ temperatureConvertion.py
@@ -1,3 +1,12 @@
 __author__ = 'zpiao1'
 
 tempLimit = float(input("Enter conversion limit for F: "))
+
+print("\tFahrenheit\tCelsius")
+print("\t----------\t-------")
+
+fahren = 32.0
+while fahren <= tempLimit:
+    celsius = (fahren - 32.0) * 5.0 / 9.0
+    print("\t  %5.1f\t\t %5.1f" % (fahren, celsius))
+    fahren += 10
